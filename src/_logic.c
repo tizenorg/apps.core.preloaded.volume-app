59234ebf962a17d7f3988e66e3e2d21fa25278d9
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-16 01:05:18 +0900

    merge with master

--- src/_logic.c
+++ src/_logic.c
@@ -950,6 +950,7 @@ int _app_reset(bundle *b, void *data)
 		retvm_if(block == NULL, -1, "Failed to add block layout\n");
 
 		edje_object_signal_callback_add(elm_layout_edje_get(block), "clicked", "*", _block_clicked_cb, ad);
+
 		outer = _add_layout(win, EDJ_APP, GRP_VOLUME_LAYOUT);
 		retvm_if(outer == NULL, -1, "Failed to add outer layout\n");
 
